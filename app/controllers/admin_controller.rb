82978371955a47a39c028d7f9ff0e197ccdc85f0
Tom Dooner <tomdooner@gmail.com>
2011-02-06 16:54:18 -0500

    Adds info to the admin page so I don't have to keep looking in the console.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,5 +3,9 @@ class AdminController < ApplicationController
 
   def index
 	  breadcrumbs.add("Admin Panel")
+	  @signups = SignUp.all
+	  @num_signups = @signups.length
+	  @team_signups = @signups.map{|x| x.team.name}.uniq
+	  @num_team_signups = @team_signups.length
   end
 end
