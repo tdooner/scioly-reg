4ae84ad1f44be68a320cbb58ce1be53ba3d2a9e6
Tom Dooner <tomdooner@gmail.com>
2011-02-18 11:39:56 -0500

    Patches registration ending bug (quick, dirty fix). Overall, the underlying problem still remains.

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -13,11 +13,15 @@ class Tournament < ActiveRecord::Base
 	def humanize()
 		return date.strftime("%B %d, %Y")
 	end
+	def human_times()
+		format1 = "%B %e, %Y @ %I:%M %p"
+		{:registration_begins => self.registration_begins.strftime(format1), :registration_ends => self.registration_ends.strftime(format1)}
+	end
 	def has_registration_begun()
 		return self.registration_begins < Time.now
 	end
 	def has_registration_ended()
-		return self.registration_ends < Time.now
+		return !(Time.now.utc + Time.zone_offset("EST") < self.registration_ends)
 	end
 	def can_register()
 		return (self.has_registration_begun() and not self.has_registration_ended())
