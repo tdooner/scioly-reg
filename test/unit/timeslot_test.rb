f862ece13190d735f2a1e60dc58e7dd574fb6a66
Tom Dooner <tomdooner@gmail.com>
2012-01-20 21:12:57 -0500

    Timeslot validation test.

--- test/unit/timeslot_test.rb
+++ test/unit/timeslot_test.rb
@@ -1,8 +1,11 @@
 require 'test_helper'
 
 class TimeslotTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "Timeslots don't save if they have no schedule, times, or capacity." do
+    event = @current_tournament.schedules.first
+    assert !Timeslot.new({:schedule => event, :begins => @current_tournament.date + 2.hours, :ends => @current_tournament.date + 2.hours + 50.minutes}).save, "Timeslot saved with no team capacity!"
+    assert !Timeslot.new({:schedule => event, :begins => @current_tournament.date + 2.hours, :team_capacity => 10}).save, "Timeslot saved with no end time!"
+    assert !Timeslot.new({:schedule => event, :ends => @current_tournament.date + 2.hours, :team_capacity => 10}).save, "Timeslot saved with no begin time!"
+    assert !Timeslot.new({:begins => @current_tournament.date + 2.hours, :ends => @current_tournament.date + 2.hours + 50.minutes, :team_capacity => 10}).save, "Timeslot saved with no schedule!"
   end
 end
