9f6b1d4e58d579341e093d7b0db45a0e71b97832
Tom Dooner <tom@causes.com>
2013-01-21 13:58:29 -0500

    Add capability to hide the right reminder bar
    By setting @right_hidden = true in the controller, the right reminder
    bar will disappear.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -13,6 +13,7 @@ class AdminController < ApplicationController
 
   def events
     breadcrumbs.add("Events")
+    @right_hidden = true
     @events = @current_tournament.schedules
     @teams_by_division = Team.divisions.inject({}) {|a,i|
       a.merge(i[0] => Team.count(:conditions=>["division = ? AND tournament_id = ?", i[0], @current_tournament]))
