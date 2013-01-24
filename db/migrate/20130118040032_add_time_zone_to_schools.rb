e6bb8b8bdd9947b1b29709e3f640f374d537e55e
Tom Dooner <tom@causes.com>
2013-01-24 12:01:11 -0500

    Remove duplicate migration
    For some reason I thought this column needed to be added. It was already
    added in a previous migration, so this migration should not have been
    existent in the first place.

--- db/migrate/20130118040032_add_time_zone_to_schools.rb
+++
@@ -1,7 +0,0 @@
-class AddTimeZoneToSchools < ActiveRecord::Migration
-  def change
-    change_table :schools do |t|
-      t.string :time_zone, :default => 'Eastern Time (US & Canada)'
-    end
-  end
-end
