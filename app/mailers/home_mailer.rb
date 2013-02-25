0abc2f62544d3bb2d490187a051587bb389a2fc8
Tom Dooner <tom@causes.com>
2013-02-25 15:39:41 -0500

    Send emails to the proper location

--- app/mailers/home_mailer.rb
+++ app/mailers/home_mailer.rb
@@ -1,11 +1,12 @@
 class HomeMailer < ActionMailer::Base
   default :from => 'Foundry <no-reply@sciolyreg.org>',
+    :bcc => 'tomdooner@gmail.com',
     :return_path => 'no-reply@sciolyreg.org'
   layout 'email'
 
   def welcome(tournament)
     @tournament = tournament
 
-    mail(:to => 'tomdooner@gmail.com')
+    mail(:to => @tournament.school.admin_email)
   end
 end
