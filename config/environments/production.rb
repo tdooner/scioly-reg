446d8bd5240e4f09c4d560cc7770300bf05ec952
Tom Dooner <tom.dooner@causes.com>
2014-01-28 00:05:13 -0800

    Use Cloudfront for assets
    A bit of an experiment, we'll see how this goes.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,7 +38,7 @@ Scioly::Application.configure do
   config.assets.digest         = true
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
+  config.action_controller.asset_host = "http://cdn.sciolyreg.org"
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
