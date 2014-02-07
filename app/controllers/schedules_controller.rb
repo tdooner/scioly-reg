7be5c36b6c94f3c2d497db84029cb5d42d38ab79
Tom Dooner <tom.dooner@causes.com>
2014-02-06 22:26:33 -0800

    Convert Schedule#show to Slim for streaming

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -143,7 +143,7 @@ class SchedulesController < ApplicationController
         render :pdf => @schedule.event.gsub(/[^a-zA-Z]/, '_') + "_" + @schedule.division
       end
       format.html do
-        render :show
+        render :show, stream: true
       end
     end
   end
