9afb9539356b7cb10eb74eaffe6ad0cf3e478d01
Tom Dooner <tom.dooner@causes.com>
2014-03-08 21:13:27 -0800

    Add nprogress.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,3 +7,8 @@
 //= require bootstrap.min
 //= require jquery.tablesorter.min
 //= require analytics
+//= require nprogress
+
+$(document).on('page:fetch',   function() { NProgress.start(); });
+$(document).on('page:change',  function() { NProgress.done(); });
+$(document).on('page:restore', function() { NProgress.remove(); });
