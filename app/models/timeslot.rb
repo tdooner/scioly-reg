378912b10b10feb4955363dd704d2a8e25ea044f
Tom Dooner <tom.dooner@causes.com>
2014-03-20 09:06:58 -0700

    Ensure timeslots have present team_capacity

--- app/models/timeslot.rb
+++ app/models/timeslot.rb
@@ -2,4 +2,6 @@ class Timeslot < ActiveRecord::Base
   belongs_to :schedule
   has_many :sign_ups, dependent: :destroy
   has_many :occupants, :through => :sign_ups, :source => 'team'
+
+  validates :team_capacity, presence: true
 end
