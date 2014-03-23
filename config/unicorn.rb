142b5d3a3c41ba791771ed08e5ff03ee90375d40
Tom Dooner <tom.dooner@causes.com>
2014-03-23 14:18:00 -0700

    Increase the concurrency a bit further
    Maybe I should have just cranked it to 11

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,5 @@
-worker_processes Integer(ENV["WEB_CONCURRENCY"] || 6)
-timeout 15
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 10)
+timeout 30
 preload_app true
 
 before_fork do |server, worker|
